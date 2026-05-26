b602a046044d260cc1e0b7e1cbc7a4d4b40cade9
803ghz <sergiodiazfp@gmail.com>
2026-05-26 19:58:02 +0200

    refactor: simplify code logic

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const express = require("express");
-
 const app = express();
 const PORT = 2001;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+})
 
 app.get('/api/health', (req, res) => {
     res.json({ status: "ok" });
 });
 
-let gastos = [
+const gastos = [
     { id: 1, concepto: 'Alquiler', cantidad: 800, categoria: 'Vivienda' },
     { id: 2, concepto: 'Supermercado', cantidad: 150, categoria: 'Alimentación' },
     { id: 3, concepto: 'Internet', cantidad: 40, categoria: 'Servicios' },
     { id: 4, concepto: 'Coche', cantidad: 30, categoria: 'Utilidad' },
-    { id: 5, concepto: 'Cena fuera', cantidad: 50, categoria: 'Ocio' }
+    { id: 5, concepto: 'Cena fuera', cantidad: 50, categoria: 'Ocio' },
+    { id: 6, concepto: 'Luz', cantidad: 50, categoria: 'Servicios' },
+    { id: 7, concepto: 'Amazon Prime', cantidad: 4.99, categoria: 'Suscripciones' },
+    { id: 8, concepto: 'PlayStation Plus', cantidad: 8.99, categoria: 'Ocio' },
 ];
 
 app.get('/api/gastos', (req, res) => {
-    res.json(gastos);
+    return res.json(gastos);
+});
+
+app.post("/api/gastos", (req, res) => {
+    console.log(req.body);
+    const { concepto, cantidad, categoria } = req.body;
+if (!concepto || !cantidad || !categoria) {
+    return res.status(400).json({ error: "falta contenido"})
+}
+
+const nuevoGasto = {
+    id: gastos.length + 1,
+    concepto,
+    cantidad,
+    categoria
+};
+
+gastos.push(nuevoGasto)
+    res.status(200).json(nuevoGasto);
 });
 
 app.get('/api/gastos/:id', (req, res) => {
@@ -33,3 +60,4 @@ app.get('/api/gastos/:id', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en ${PORT}`);
 });
+
