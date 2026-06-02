d77f8342361459e21b6230b4de843c4559c850ba
803ghz <sergiodiazfp@gmail.com>
2026-06-02 19:27:14 +0200

    feat: integrate a validation middleware into routes

--- src/routes/gastos.routes.js
+++ src/routes/gastos.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { obtenerGastos, obtenerGastoPorId, crearGasto, actualizarGasto, borrarGasto } = require('../controllers/gastos.controller');
+const validate = require("../middlewares/validate")
+const { body, validationResult } = require("express-validator");
 
 router.get('/', obtenerGastos);
 router.get('/:id', obtenerGastoPorId);
-router.post('/', crearGasto);
+router.post('/', body("concepto").notEmpty().withMessage("El concepto es obligatorio"), validate, crearGasto);
 router.put('/:id', actualizarGasto);
 
 module.exports = router;
