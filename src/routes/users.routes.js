d7e0282c32d92a9244071dafd3440e75d6a18337
803ghz <sergiodiazfp@gmail.com>
2026-06-03 19:06:14 +0200

    feat: implement validation and registerRules routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require("../controllers/users.controller")
-router.post("/register", usersController)
+const registerRules = require("../middlewares/registerRules")
+const validate = require("../middlewares/validate")
+
+router.post("/register", validate, usersController)
 
 module.exports = router
