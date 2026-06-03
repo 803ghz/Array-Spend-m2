1d74e61ee8c1f7db71db26fd6a1b6dc163cc9b14
803ghz <sergiodiazfp@gmail.com>
2026-06-03 16:36:28 +0200

    feat: add user Routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const gastosRoutes = require('./routes/gastos.routes');
+const usersRoutes = require("../src/routes/users.routes")
 
 app.use(express.json());
 
 app.use('/api/gastos', gastosRoutes);
+app.use('/api/gastos', usersRoutes);
 
 app.get("/api/health", (req, res) => res.json({ status: "ok" }));
 
