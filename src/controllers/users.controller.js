f4b57fd77afeb50b411e640b3ad250bf1a7c8b7a
803ghz <sergiodiazfp@gmail.com>
2026-06-05 16:52:55 +0200

    refactor: clean up codebase and fix minor bugs

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,29 +3,54 @@ const Users = require("../models/users.models.js")
 
 async function registerUser(req, res) {
     try {
-    const { email, password } = req.body;
+        const { email, password } = req.body;
 
-    const existEmail = Users.validateEmail(email);
+        const emailChecked = await Users.validateEmail(email)
 
-        if (existEmail) {
+        if (emailChecked) {
             return res.status(409).json({ error: "Este email ya ha sido registrado" });
         }
 
         const passwordHashed = await bcrypt.hash(password, 10);
-
-        const newUser = await Users.createUser({
-            email, password: passwordHashed
-        })
+        const newUser = await Users.createUser({ email, password: passwordHashed });
 
         res.status(201).json({
             id: newUser._id,
             email: newUser.email,
-        })
-
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Error en el registro" });
     }
 }
 
-module.exports = registerUser
+async function loginUser(req, res) {
+    try {
+        const { email, password } = req.body;
+        const user = await Users.findUserByEmail(email);
+
+        if (!user) {
+            return res.status(401).json({ error: "Credenciales incorrectas" });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+            return res.status(401).json({ error: "Credenciales incorrectas" });
+        }
+
+        res.status(200).json({
+            message: "Login correcto",
+            id: user._id,
+            email: user.email,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error en el login" });
+    }
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+};
