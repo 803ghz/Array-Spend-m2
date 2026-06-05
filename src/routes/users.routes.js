3f98d45f78ce790493192b1654577c54a125f0c9
803ghz <sergiodiazfp@gmail.com>
2026-06-05 17:01:06 +0200

    refactor: reorganize user routes and middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const usersController = require("../controllers/users.controller")
 const registerRules = require("../middlewares/registerRules")
+const loginRules = require("../middlewares/loginRules")
 const validate = require("../middlewares/validate")
 
-router.post("/register", validate, usersController)
+router.post(
+	"/register",
+	registerRules,
+	validate,
+	usersController.registerUser
+)
+
+router.post(
+    "/login",
+    loginRules,
+    validate,
+    usersController.loginUsergit add
+)
 
 module.exports = router
