86809a1d1f2d5de9dfc562ab5ab19b5c094d1476
803ghz <sergiodiazfp@gmail.com>
2026-06-26 16:36:43 +0200

    fix: solve minor problems with CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import gastosRoutes from './routes/gastos.routes.js';
 import usersRoutes from "./routes/users.routes.js";
+import cors from "cors"
 
 const app = express();
+const cors = require("cors");
+
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api/gastos', gastosRoutes);
 app.use('/api/users', usersRoutes);
