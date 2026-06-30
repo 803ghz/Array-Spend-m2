fb04dd54c3d7086553e8e9e90bf11c7e49156a1a
803ghz <sergiodiazfp@gmail.com>
2026-06-30 12:15:05 +0200

    fix: minor bug on app.js

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+    console.log(`${req.method} ${req.url}`);
+    next();
 });
 
 app.use("/api/gastos", gastosRoutes);
